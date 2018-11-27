38d3a544fca2d894a2b929d2afafc2049756dc49
Usama Naseer <usaman1993@gmail.com>
2018-11-27 22:03:10 +0500

    west Pointed

--- OnlyForBuild/utils/constants.js
+++ OnlyForBuild/utils/constants.js
@@ -1,7 +1,7 @@
 var dev = "http://websvc.westus.cloudapp.azure.com";
 var prod = "https://api.pdn.netpace.net";
-var west = "https://codesapiqtstageweb.netpace.com/";
-var web  = "https://codesapiweb.coupons.com";
+var central = "https://codesapiqtstageweb.netpace.com/";
+var west  = "https://codesapiweb.coupons.com";
 var qt = "https://codesapiqtstageweb.netpace.com"
 
 var baseUrl = west;
